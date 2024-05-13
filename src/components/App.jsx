5b6222f3f421916a98c8c03f86e74086b0ad9150
Pi Rey <pilar.rrdgz@gmail.com>
2024-05-13 11:18:24 +0200

    move rollDice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ import Dice from "./Dice";
 import Container from "./Container";
 import BtnReset from "./BtnReset";
 
-
 function App() {
   //Crear funciones de estado
   const [grogu, setGrogu] = useState(0); //Es un 0 porque es la posición de índice de dónde estará en la matriz
@@ -17,15 +16,55 @@ function App() {
   const [dice, setDice] = useState(null); //OJO: es un null (sin valor) pero luego tendrá valor numérico, almacenará el número random
   const [gameStatus, setGameStatus] = useState("En curso");
 
+//   function getRandomNumber(max) {
+//     max = 4;
+//     return Math.ceil(Math.random() * max);
+//   }
+
+  const handleRollDice = () => {
+   const value = Math.floor(Math.random() * 4);
+    setDice(value);
+    console.log(dice);
+
+    if (dice === 1) {
+      //eliminar 1 galleta: splice? de posición 0
+      //comprobación de length array - cambiar mensaje por ejemplo
+      //"¡Has descargado una galleta!"
+      //HAY QUE LIMITAR A 3 VECES
+      console.log("¡Has descargado una galleta!");
+    } else if (dice === 2) {
+      //eliminar 1 huevo: splice? de posición 0
+      //comprobación de length array - cambiar mensaje por ejemplo
+      //"¡Has descargado un huevo!"
+      //HAY QUE LIMITAR A 3 VECES
+      console.log("¡Has descargado un huevo!");
+    } else if (dice === 3) {
+      //eliminar 1 rana: splice? de posición 0
+      //comprobación de length array - cambiar mensaje por ejemplo
+      //"¡Has descargado una rana!"
+      //HAY QUE LIMITAR A 3 VECES
+      console.log("¡Has descargado una rana!");
+    } else if (dice === 4) {
+      //grogu avanza 1 casilla
+      //"¡Grugo ha avanzado una casilla!"
+      //HAY QUE LIMITAR A 6 VECES
+      console.log("¡Grugo ha avanzado una casilla!");
+    }
+   
+    //comprobación game status: mirar length de cada array.
+
+    //re-renderizar grogu en su casilla si cambia de posición y mercancía si ha cambiado
+  };
+
   return (
     <>
       <div className="page">
-        <Header/>
+        <Header />
         <main className="page">
-          <Board/>
-          <Dice />
-          <Container/>
-          <BtnReset/>
+          <Board />
+          <Dice handleRollDice={handleRollDice} />
+          <Container />
+          <BtnReset />
         </main>
       </div>
     </>
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,61 +1,23 @@
 import "../styles/Dice.scss";
-import { useState } from "react";
+import PropTypes from "prop-types";
 
-const Dice = () => {
-
-  const [dice, setDice] = useState(null);
-
-  function getRandomNumber(max) {
-    max = 4;
-    return Math.ceil(Math.random() * max);
-  }
-
-  const handleRollDice =(ev)=>{
+const Dice = ({ handleRollDice }) => {
+  const handleDice = (ev) => {
     ev.preventDefault();
-    setDice(getRandomNumber());
-    console.log(dice);
-
-    if(dice === 1){
-      //eliminar 1 galleta: splice? de posición 0
-      //comprobación de length array - cambiar mensaje por ejemplo
-      //"¡Has descargado una galleta!"
-      //HAY QUE LIMITAR A 3 VECES
-      console.log("¡Has descargado una galleta!");
-    } else if (dice === 2){
-      //eliminar 1 huevo: splice? de posición 0
-      //comprobación de length array - cambiar mensaje por ejemplo
-      //"¡Has descargado un huevo!"
-      //HAY QUE LIMITAR A 3 VECES
-      console.log("¡Has descargado un huevo!");
-    } else if (dice === 3){
-      //eliminar 1 rana: splice? de posición 0
-      //comprobación de length array - cambiar mensaje por ejemplo
-      //"¡Has descargado una rana!"
-      //HAY QUE LIMITAR A 3 VECES
-      console.log("¡Has descargado una rana!");
-    } else if (dice === 4){
-      //grogu avanza 1 casilla
-      //"¡Grugo ha avanzado una casilla!" 
-      //HAY QUE LIMITAR A 6 VECES
-      console.log("¡Grugo ha avanzado una casilla!");
-    }
-
-
-
-    //comprobación game status: mirar length de cada array.
-
-  
-    //re-renderizar grogu en su casilla si cambia de posición y mercancía si ha cambiado
-
-  }
+    handleRollDice();
+  };
 
   return (
     <section>
-        <button className="dice" onClick={handleRollDice}>Lanzar Dado</button>{" "}
-        {/* cuando sea el momento: desactivar este botón */}
-        <div className="game-status">En curso</div>
+      <button className="dice" onClick={handleDice}>
+        Lanzar Dado
+      </button>{" "}
+      {/* cuando sea el momento: desactivar este botón */}
+      <div className="game-status">En curso</div>
     </section>
-  )
-}
+  );
+};
 
-export default Dice
+Dice.propTypes = { handleRollDice: PropTypes.func };
+
+export default Dice;
